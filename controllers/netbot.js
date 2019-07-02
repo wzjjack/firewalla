48926093773424a4d6980df789c9ae0120a6dcba
wzjjack <firewalla.zhijie@gmail.com>
2019-07-02 14:55:47 +0800

    update

--- controllers/netbot.js
+++ controllers/netbot.js
@@ -3836,7 +3836,7 @@ class netBot extends ControllerBot {
   msgHandler(gid, rawmsg, callback) {
     log.info("zhijie1559")
     log.info(dnsTool.getDNSKey("182.61.200.7"))
-    log.info(dnsTool.getDns("182.61.200.7"))
+    log.info(await dnsTool.getDns("182.61.200.7"))
     log.info("zhijie1559")
     if (rawmsg.mtype === "msg" && rawmsg.message.type === 'jsondata') {
 
