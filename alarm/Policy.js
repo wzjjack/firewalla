55327254332a176924fc32c94f8507e2ab40cb1c
child <firewalla.zhijie@gmail.com>
2020-12-04 15:58:29 +0800

    update

--- alarm/Policy.js
+++ alarm/Policy.js
@@ -155,7 +155,15 @@ class Policy {
     }
     if (!(policy instanceof Policy))
       policy = new Policy(policy) // leverage the constructor for compatibilities conversion
-
+      
+    if (this.related_screen_time_pid === policy.related_screen_time_pid &&
+      this.target === this.target &&
+      this.type === this.type &&
+      arraysEqual(this.scope, policy.scope) &&
+      arraysEqual(this.tag, policy.tag)
+    ) {
+      return true;
+    }
     if (
       this.type === policy.type &&
       this.target === policy.target &&
