230542b675398e983230e5fe83ebaf85f5ddfdd4
child <firewalla.zhijie@gmail.com>
2021-08-18 18:25:08 +0800

    update

--- util/TimeSeries.js
+++ util/TimeSeries.js
@@ -22,8 +22,8 @@ timeSeries.granularities = {
   '1minute'  : { ttl: timeSeries.minutes(65)  , duration: timeSeries.minutes(1) },
   '15minutes': { ttl: timeSeries.hours(50)  , duration: timeSeries.minutes(15) },
   '1hour'    : { ttl: timeSeries.days(7)   , duration: timeSeries.hours(1) },
-  '1day'     : { ttl: timeSeries.weeks(52) , duration: timeSeries.days(1) },
-  '1month'   : { ttl: timeSeries.months(24) , duration: timeSeries.months(1) }
+  '1day'     : { ttl: timeSeries.weeks(52) , duration: timeSeries.days(1) }, // can be removed in future
+  '1month'   : { ttl: timeSeries.months(24) , duration: timeSeries.months(1) } // can be removed in future
 }
 
 const boneAPITimeSeries = new TimeSeries(rclient, "boneAPIUsage")
@@ -33,7 +33,30 @@ boneAPITimeSeries.granularities = {
   '1day'     : { ttl: boneAPITimeSeries.days(30) , duration: boneAPITimeSeries.days(1) },
 }
 
+const timeSeriesWithTz = new TimeSeries(rclient, "timedTrafficWithTz")
+timeSeriesWithTz.granularities = {
+  '1day': { ttl: timeSeries.weeks(52), duration: timeSeries.days(1) },
+  '1month': { ttl: timeSeries.months(24), duration: timeSeries.months(1) }
+}
+const timeSeriesWithTzBeginingKey = "time:series:with:tz:ts"
+
 module.exports = {
-  getTimeSeries: function() {return timeSeries},
-  getBoneAPITimeSeries: function() {return boneAPITimeSeries}
+  getTimeSeries: function () { return timeSeries },
+  getBoneAPITimeSeries: function () { return boneAPITimeSeries },
+  getTimeSeriesWithTz: function () {
+    if ((await rclient.existsAsync(timeSeriesWithTzBeginingKey)) != 1) {
+      await rclient.setAsync(timeSeriesWithTzBeginingKey, new Date() / 1000)
+    }
+    return timeSeriesWithTz
+  },
+  supportTimeSeriesWithTz: function () {
+    if ((await rclient.keysAsync("timedTraffic:download:1minute*")).length < 2) {
+      // it is very new box which install within 65 mins
+      return true
+    }
+    if ((await rclient.existsAsync(timeSeriesWithTzBeginingKey)) != 1) return false
+    const beginningTs = await rclient.getAsync(timeSeriesWithTzBeginingKey)
+    if (new Date() / 1000 - beginningTs > 31 * 24 * 60 * 60) return true
+    return false
+  }
 }
