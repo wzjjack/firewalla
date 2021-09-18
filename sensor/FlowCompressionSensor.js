da6823381d45edf7a11ffb72425d9ddfba7d296f
child <firewalla.zhijie@gmail.com>
2021-09-18 16:26:08 +0800

    update

--- sensor/FlowCompressionSensor.js
+++ sensor/FlowCompressionSensor.js
@@ -26,6 +26,7 @@ const extensionManager = require('./ExtensionManager.js')
 const { rclient } = require('../util/redis_manager');
 const deflateAsync = Promise.promisify(zlib.deflate);
 const sem = require('./SensorEventManager.js').getInstance();
+const MAX_MEM = 10 * 1000 * 1000
 
 class FlowCompressionSensor extends Sensor {
   constructor() {
@@ -33,15 +34,26 @@ class FlowCompressionSensor extends Sensor {
     this.recentlyTickKey = "compressed:flows:lastest:ts"
     this.step = 60 * 60 // one hour
     this.maxInterval = 24 * 60 * 60 // 24 hours
+    this.compressedMem = 0
   }
 
   async run() {
     sem.once('IPTABLES_READY', async () => {
+      await this.calMem()
       await this.build()
       setInterval(async () => {
         await this.build()
       }, 30 * 60 * 1000);
     })
+  }
+
+  async calMem() {
+    const compressedFlowsKeys = await rclient.scanResults(this.getKey("*", "*"))
+    for (const key of compressedFlowsKeys) {
+      const mem = Number(await rclient.memoryAsync("usage", key) || 0)
+      this.compressedMem += mem
+    }
+    log.info("jack test this.compressedMem", this.compressedMem)
   }
 
   async apiRun() {
@@ -114,8 +126,12 @@ class FlowCompressionSensor extends Sensor {
   async save(begin, end, flows) { // might save to disk in future
     const base64Str = await this.compress(flows)
     const key = this.getKey(begin, end)
+    this.compressedMem += base64Str.length
     await rclient.setAsync(key, base64Str)
-    await rclient.expireatAsync(key, end + this.maxInterval)
+    log.info("jack test this.compressedMem", this.compressedMem)
+    // reduce ttl if compressedMem bigger than MAX_MEM
+    const ttl = this.compressedMem > MAX_MEM ? this.maxInterval / 4 : this.maxInterval
+    await rclient.expireatAsync(key, end + ttl)
   }
 
   async getBuildingWindow() {
