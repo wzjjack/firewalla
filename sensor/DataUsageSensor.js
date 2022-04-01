5c3d478ee5fb24ad50b08ac2ee0856267372aff1
child <firewalla.zhijie@gmail.com>
2022-04-01 13:11:28 +0800

    bugfix

--- sensor/DataUsageSensor.js
+++ sensor/DataUsageSensor.js
@@ -261,7 +261,6 @@ class DataUsageSensor extends Sensor {
         sem.on('DataPlan:Updated', async (event) => {
             const date = event && event.date;
             if (date) {
-                await this.cleanMonthlyDataUsage();
                 await this.generateLast12MonthDataUsage(date);
                 this.cornJob && this.cornJob.stop();
                 this.cornJob = new CronJob(`0 0 0 ${date} * *`, async () => {
@@ -278,8 +277,9 @@ class DataUsageSensor extends Sensor {
     }
 
     async generateLast12MonthDataUsage(planDay) {
-        const lastTs = await rclient.getAsync('monthly:data:usage:lastTs');
-        log.info(`Going to generate monthly data usage, plan day ${planDay}, lastTs ${lastTs}`);
+        log.info(`Going to generate monthly data usage, plan day ${planDay}`);
+        // clean old data
+        await this.cleanMonthlyDataUsage();
         const now = new Date();
         const days = now.getDate(), month = now.getMonth(),
             year = now.getFullYear();
@@ -296,7 +296,6 @@ class DataUsageSensor extends Sensor {
             } else {
                 recordTs = new Date(year, month - i, planDay);
             }
-            if (recordTs < lastTs * 1000) break;
             const offsetDays = Math.floor((today - recordTs) / oneDay) + hostManager.offsetSlot();
             const download = await getHitsAsync(downloadKey, '1day', offsetDays) || [];
             const upload = await getHitsAsync(uploadKey, '1day', offsetDays) || [];
@@ -326,7 +325,6 @@ class DataUsageSensor extends Sensor {
 
     async dumpToRedis(records) {
         // monthly:data:usage:ts
-        // monthly:data:usage:lastTs
         const multi = rclient.multi();
         const expiring = 60 * 60 * 24 * 365; // one year
         for (const record of records) {
@@ -334,7 +332,6 @@ class DataUsageSensor extends Sensor {
             multi.set(key, JSON.stringify(record));
             multi.expireat(key, record.ts + expiring);
         }
-        multi.set('monthly:data:usage:lastTs', records[0].ts);
         await multi.execAsync();
     }
     getStats(stats, days) {
@@ -347,7 +344,6 @@ class DataUsageSensor extends Sensor {
         const keys = await rclient.scanResults("monthly:data:usage:*");
         let records = [];
         for (const key of keys) {
-            if (key == "monthly:data:usage:lastTs") continue;
             try {
                 const record = await rclient.getAsync(key);
                 records.push(JSON.parse(record));
