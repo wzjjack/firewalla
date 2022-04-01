aa1c0f37af89c618e96c6f85af3b38b358bfde6c
child <firewalla.zhijie@gmail.com>
2022-04-01 13:29:22 +0800

    update

--- sensor/DataUsageSensor.js
+++ sensor/DataUsageSensor.js
@@ -296,7 +296,7 @@ class DataUsageSensor extends Sensor {
             } else {
                 recordTs = new Date(year, month - i, planDay);
             }
-            if (recordTs > today) continue;
+            if (recordTs > today) continue; // if monthly begin ts later than today, skip
             const offsetDays = Math.floor((today - recordTs) / oneDay) + hostManager.offsetSlot();
             const download = await getHitsAsync(downloadKey, '1day', offsetDays) || [];
             const upload = await getHitsAsync(uploadKey, '1day', offsetDays) || [];
@@ -310,7 +310,7 @@ class DataUsageSensor extends Sensor {
                 records.push({ ts: recordTs / 1000, stats: stats })
             }
         }
-        if (days > planDay) {
+        if (days > planDay) { // the first record is not completed
             records.shift();
         }
         await this.dumpToRedis(records);
