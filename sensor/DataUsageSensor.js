dec105ba220f519782812a91b415ba851beb6ad5
child <firewalla.zhijie@gmail.com>
2022-04-01 13:20:28 +0800

    update

--- sensor/DataUsageSensor.js
+++ sensor/DataUsageSensor.js
@@ -296,6 +296,7 @@ class DataUsageSensor extends Sensor {
             } else {
                 recordTs = new Date(year, month - i, planDay);
             }
+            if (recordTs > today) continue;
             const offsetDays = Math.floor((today - recordTs) / oneDay) + hostManager.offsetSlot();
             const download = await getHitsAsync(downloadKey, '1day', offsetDays) || [];
             const upload = await getHitsAsync(uploadKey, '1day', offsetDays) || [];
